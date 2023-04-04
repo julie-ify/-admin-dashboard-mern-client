f771e9d974df37738a62cb999da6db13f2edab3b
Juliana Ifionu <julieifionu@gmail.com>
2023-04-04 00:59:02 -0400

    Fix bugs causing errors

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ const store = configureStore({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<Provider>
-			<App store={store} />
+		<Provider store={store}>
+			<App />
 		</Provider>
 	</React.StrictMode>
 );
