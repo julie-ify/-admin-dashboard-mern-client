1900a4b275d2eb620f2d0274280060fa0e28ac78
Juliana Ifionu <julieifionu@gmail.com>
2023-04-04 00:42:04 -0400

    Configure the redux store for light/dark mode

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { configureStore } from '@reduxjs/toolkit';
+// globaReducer is an alias for globalSlice. We could name it anything since its a default export.
+import globalReducer from 'state'; // The jsconfig allows us to use 'state' instead of './state'. This is called absolute path
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+	reducer: {
+		global: globalReducer,
+	},
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider>
+			<App store={store} />
+		</Provider>
+	</React.StrictMode>
 );
